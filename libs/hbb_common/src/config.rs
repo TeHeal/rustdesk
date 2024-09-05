6890f1b507bb1b59fb9cf2a209b88e6970f2b582
一恺 <132058077+TeHeal@users.noreply.github.com>
2024-09-05 15:06:38 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.teheal.cn:1106"];
+pub const PUBLIC_RS_PUB_KEY: &str = "lMVuZ5Srwdz+YRGR2gpX5AHR0WU9PBb3cxcrZVbM0fA=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
